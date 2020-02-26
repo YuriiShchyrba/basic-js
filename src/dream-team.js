705ccc38aa81b04fe467e865c511eca5f5b8ac2b
Yurii Shchyrba <yurashchyrba@gmail.com>
2020-02-26 18:23:27 -0500

    dsd

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,20 +1,24 @@
 module.exports = function createDreamTeam(str) {
-  var arr ="";
-  for (var i = 0; i < str.length; i++) {
-    str[i] = str[i].toUpperCase();
-    str[i] = str[i].replace(/ /g, "");
-}
-
-
-  for ( var i = 0 ; i < str.length ; i++)
+  if( Array.isArray(str))
   {
-    if ( str[i][0].match(/[A-Z]/i) )
-    {
-        arr+=str[i][0];
-    }
+    var arr ="";
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].toUpperCase();
+      str[i] = str[i].replace(/ /g, "");
   }
-
- arr =arr.split("").sort().join("") ;
-  if ( arr.length == 0) { return false;}
-  return arr;
+  
+  
+    for ( var i = 0 ; i < str.length ; i++)
+    {
+      if ( str[i][0].match(/[A-Z]/i) )
+      {
+          arr+=str[i][0];
+      }
+    }
+  
+   arr =arr.split("").sort().join("") ;
+    if ( arr.length == 0) { return false;}
+    return arr;
+  }
+  return false;
 };
