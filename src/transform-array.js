dbd6e101b658228fe036f876750246c6be50fe83
Yurii Shchyrba <yurashchyrba@gmail.com>
2020-03-09 02:08:31 -0400

    dsds

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,27 @@
 module.exports = function transform( arr ) {
     if( !Array.isArray(arr)) throw new Error;
-
-    return true;
+    var res=[];
+    for ( var i = 0 ; i < arr.length; i++)
+    {
+        switch (arr[i])
+        {
+            case "--discard-next":
+                i++;
+                break;
+            case "--discard-prev":
+                res.pop();
+                break;
+            case "--double-next":
+                res.push(arr[i+1]);
+                break;
+            case "--double-prev":
+                res.push(arr[i-1]);
+                break;
+            default:
+                res.push(arr[i]);
+                break;
+        }
+    }
+    return res;
 };
+
